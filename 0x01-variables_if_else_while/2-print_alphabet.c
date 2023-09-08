e3e6443534c3f614205305bf68820bd636c1c848
Manalamay <amaymanal@gmail.com>
2023-09-08 10:23:48 -0700

    last digit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - main function
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	char n;
+	for( n = 'a' ; n <= 'z' ; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
