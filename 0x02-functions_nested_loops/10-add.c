b3000d51a3f204f43f7bc83012a15a82ba6cb948
Manalamay <amaymanal@gmail.com>
2023-09-13 09:31:50 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - function that adds two integers
+ * @a: function charachter 1
+ * @b: function charachter
+ * Return: a + b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
