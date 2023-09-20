6390ad2cd90915e470448a4cfef7d188d692c6fa
Manalamay <amaymanal@gmail.com>
2023-09-20 14:50:49 -0700

    focus

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strcat - ffunction that concatenates two strings
+ * _strcat - concatenates two string
  * @dest: destination string
  * @src: source string
  * Return: dest
@@ -13,9 +13,9 @@ char *_strcat(char *dest, char *src)
 	int srclen = 0;
 	int i;
 
-	for (i = 0 ; destlen != '\0' ; i++)
+	for (i = 0 ; dest[i]  != '\0' ; i++)
 		destlen++;
-	for (i = 0 ; srclen != '\0' ; i++)
+	for (i = 0 ; src[i] != '\0' ; i++)
 		srclen++;
 	for (i = 0 ; i <= srclen ; i++)
 		dest[destlen + i] = src[i];
